0bfa5106b26e69491d5e92f9088ac84ee3818376
Роман Данилевич <dwordchanel@gmail.com>
2020-06-05 00:36:46 +0300

    Add project files.

---
+++ Lab 3.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Lab_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            { 
+                Console.Write("Вкажіть довжину масиву: ");
+                int len = int.Parse(Console.ReadLine());
+                Console.Write("Введіть n: ");
+                int n = int.Parse(Console.ReadLine());
+
+                int[] arr = new int[len];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write("Вкажіть наступний елемент: ");
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+
+                System.IO.File.WriteAllText("numbers.txt", string.Join(" ", arr));
+
+                int count = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > n) count++;
+                }
+                Console.WriteLine($"Кількість чисел, що перевищує {n} є {count}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
